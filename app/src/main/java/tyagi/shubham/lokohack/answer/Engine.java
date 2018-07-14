ada403dd9d883165519d5bc9f659f4c57687a280
Shubham <shubhamtyagi.bajoat@gmail.com>
2018-07-14 12:08:39 +0530

    Create Engine.java

---
+++ app/src/main/java/tyagi/shubham/lokohack/answer/Engine.java
@@ -0,0 +1,30 @@
+package ai.loko.hk.ui.answers;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+import ai.loko.hk.ui.data.Data;
+import ai.loko.hk.ui.model.Question;
+
+import static ai.loko.hk.ui.data.Data.USER_AGENT;
+import static ai.loko.hk.ui.data.Data.delete;
+import static ai.loko.hk.ui.utils.Utils.count;
+import static ai.loko.hk.ui.utils.Utils.getSimplifiedQuestion;
+import static ai.loko.hk.ui.utils.Utils.getSimplifiedString;
+import static ai.loko.hk.ui.utils.Utils.stringToArrayList;
+
+public class Engine extends Base {
+    private final String TAG = "Engine";
+
+    public Engine(Question questionObj) {
+        super(questionObj);
+    }
+    //OTHERS CODE ARE HERE FILL FORM
+    }
