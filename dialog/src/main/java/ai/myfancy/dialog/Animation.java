389faacbcf9453dbf4897c460bf0d64ef43b859d
Shubham Tyagi <shubhamtyagi.bajoat@gmail.com>
2018-07-14 13:42:29 +0530

    fancy dialog

---
+++ dialog/src/main/java/ai/myfancy/dialog/Animation.java
@@ -0,0 +1,9 @@
+package ai.myfancy.dialog;
+
+/**
+ * Created by Administrator on 11/20/2017.
+ */
+
+public enum Animation {
+    POP,SIDE,SLIDE
+}
---
+++ dialog/src/main/java/ai/myfancy/dialog/FancyAlertDialogListener.java
@@ -0,0 +1,9 @@
+package ai.myfancy.dialog;
+
+/**
+ * Created by Administrator on 11/20/2017.
+ */
+
+public interface FancyAlertDialogListener {
+    void OnClick();
+}
